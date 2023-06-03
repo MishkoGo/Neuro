77fb84d7df5b7165a3d9a3d4eaac3175b722d9d5
MishkoGo <103243560+MishkoGo@users.noreply.github.com>
2023-06-03 22:06:07 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
 let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.add('active');
+   loginForm.classList.add('active');
 }
 
 document.querySelector('#close-login-form').onclick = () =>{
-    loginForm.classList.remove('active');
+   loginForm.classList.remove('active');
 }
 
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .nav');
+let login_tn = document.querySelector('.header #login-tn');
 
-// menu.onclick = () => {
-//     menu.classList.toggle('fa-times');
-//     navbar.classList.toggle('active');
-// }
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+   login_tn.classList.toggle('fa-times');
+}
 
-window.onscroll = () => {
-    loginForm.classList.remove('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+window.onscroll = () =>{
+   loginForm.classList.remove('active');
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
 
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }
-    else{
-        document.querySelector('.header').classList.remove('active');
-    }
+   if(window.scrollY > 0){
+      document.querySelector('.header').classList.add('active');
+   }else{
+      document.querySelector('.header').classList.remove('active');
+   }
 }
